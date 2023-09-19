efd640888dce53138038b9c00755a0ec83132e61
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-09-19 07:30:00 -0400

    Move album copy logic into function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,105 @@
+use audiotags::Tag;
+use fs_extra::dir::CopyOptions;
 use std::{
     env,
     fs::{self},
-    path::Path,
+    path::{Path, PathBuf},
+    time::SystemTime,
 };
 
-use audiotags::Tag;
-use fs_extra::dir::CopyOptions;
+fn copy_album_dir_contents(target_dir: &str, path: PathBuf) -> Option<String> {
+    let flacs = fs::read_dir(path.clone())
+        .unwrap()
+        .filter_map(|p| p.ok())
+        .filter(|p| p.file_name().to_str().unwrap().ends_with(".flac"))
+        .collect::<Vec<_>>();
+
+    if flacs.is_empty() {
+        println!(
+            "Encountered empty directory {}",
+            path.clone().to_str().unwrap()
+        );
+        return None;
+    }
+
+    let flac = flacs.first().unwrap();
+
+    let tag = Tag::new()
+        .read_from_path(flac.path().to_str().unwrap())
+        .unwrap();
+
+    let title = tag.title().unwrap();
+    let album = tag.album_title().unwrap_or("(none)");
+    let artist = tag.artist().or(tag.album_artist()).unwrap();
+    let album_dir_name = path.file_name().unwrap().to_str().unwrap();
+
+    println!("====== {} ======", path.clone().to_str().unwrap());
+    println!("title: {}", title);
+    println!("album title: {}", album);
+    println!("album directory name: {}", album_dir_name);
+    println!("artist: {}", artist);
+
+    let artist_dir = Path::new(&target_dir).join(artist);
+
+    if !artist_dir.is_dir() {
+        println!("Creating artist dir {}", artist_dir.to_str().unwrap());
+        let _ = fs::create_dir(artist_dir.clone());
+    }
+
+    let album_dir = artist_dir.join(album_dir_name);
+
+    let existing_flacs = if album_dir.is_dir() {
+        fs::read_dir(album_dir.clone())
+            .unwrap()
+            .filter_map(|p| p.ok())
+            .filter(|p| p.file_name().to_str().unwrap().ends_with(".flac"))
+            .collect::<Vec<_>>()
+    } else {
+        vec![]
+    };
+
+    if flacs.len() > existing_flacs.len() {
+        let source = path.to_str().unwrap().clone();
+        let target = artist_dir.to_str().unwrap().clone();
+        println!("Copying album dir {} -> {}", source, target);
+        if !album_dir.is_dir() {
+            let _ = fs_extra::dir::copy(source, target, &CopyOptions::new());
+            Some(String::from(album_dir.to_str().unwrap()))
+        } else {
+            let copied_files = fs::read_dir(path.clone())
+                .unwrap()
+                .filter_map(|p| p.ok())
+                .filter_map(|source| {
+                    let target = Path::new(album_dir.to_str().unwrap())
+                        .join(source.path().file_name().unwrap().to_str().unwrap());
+
+                    if target.is_file() {
+                        None
+                    } else {
+                        Some((source.path(), target))
+                    }
+                })
+                .map(|(source, target)| {
+                    let track_source = source.to_str().unwrap();
+                    let track_target = target.to_str().unwrap();
+                    let _ = fs::copy(track_source, track_target);
+                    format!("\t{}", source.file_name().unwrap().to_str().unwrap())
+                })
+                .collect::<Vec<_>>();
+
+            Some(format!(
+                "{}\n{}",
+                album_dir.to_str().unwrap(),
+                copied_files.join("\n"),
+            ))
+        }
+    } else {
+        None
+    }
+}
 
 fn main() {
+    let start = SystemTime::now();
     let args: Vec<String> = env::args().collect();
 
     let source_dir = if args.len() > 1 {
@@ -21,103 +113,12 @@ fn main() {
         panic!("Must pass a target_dir argument");
     };
 
-    let copy_options = CopyOptions::new();
-
     let updated = fs::read_dir(source_dir)
         .unwrap()
         .filter_map(|p| p.ok())
         .filter(|p| p.metadata().unwrap().is_dir())
         .map(|p| p.path())
-        .filter_map(|path| {
-            let flacs = fs::read_dir(path.clone())
-                .unwrap()
-                .filter_map(|p| p.ok())
-                .filter(|p| p.file_name().to_str().unwrap().ends_with(".flac"))
-                .collect::<Vec<_>>();
-
-            if flacs.is_empty() {
-                println!(
-                    "Encountered empty directory {}",
-                    path.clone().to_str().unwrap()
-                );
-                return None;
-            }
-
-            let flac = flacs.first().unwrap();
-
-            let tag = Tag::new()
-                .read_from_path(flac.path().to_str().unwrap())
-                .unwrap();
-
-            let title = tag.title().unwrap();
-            let album = tag.album_title().unwrap_or("(none)");
-            let artist = tag.artist().or(tag.album_artist()).unwrap();
-            let album_dir_name = path.file_name().unwrap().to_str().unwrap();
-
-            println!("====== {} ======", path.clone().to_str().unwrap());
-            println!("title: {}", title);
-            println!("album title: {}", album);
-            println!("album directory name: {}", album_dir_name);
-            println!("artist: {}", artist);
-
-            let artist_dir = Path::new(&target_dir).join(artist);
-
-            if !artist_dir.is_dir() {
-                println!("Creating artist dir {}", artist_dir.to_str().unwrap());
-                let _ = fs::create_dir(artist_dir.clone());
-            }
-
-            let album_dir = artist_dir.join(album_dir_name);
-
-            let existing_flacs = if album_dir.is_dir() {
-                fs::read_dir(album_dir.clone())
-                    .unwrap()
-                    .filter_map(|p| p.ok())
-                    .filter(|p| p.file_name().to_str().unwrap().ends_with(".flac"))
-                    .collect::<Vec<_>>()
-            } else {
-                vec![]
-            };
-
-            if flacs.len() > existing_flacs.len() {
-                let source = path.to_str().unwrap().clone();
-                let target = artist_dir.to_str().unwrap().clone();
-                println!("Copying album dir {} -> {}", source, target);
-                if !album_dir.is_dir() {
-                    let _ = fs_extra::dir::copy(source, target, &copy_options);
-                    Some(String::from(album_dir.to_str().unwrap()))
-                } else {
-                    let copied_files = fs::read_dir(path.clone())
-                        .unwrap()
-                        .filter_map(|p| p.ok())
-                        .filter_map(|source| {
-                            let target = Path::new(album_dir.to_str().unwrap())
-                                .join(source.path().file_name().unwrap().to_str().unwrap());
-
-                            if target.is_file() {
-                                None
-                            } else {
-                                Some((source.path(), target))
-                            }
-                        })
-                        .map(|(source, target)| {
-                            let track_source = source.to_str().unwrap();
-                            let track_target = target.to_str().unwrap();
-                            let _ = fs::copy(track_source, track_target);
-                            format!("\t{}", source.file_name().unwrap().to_str().unwrap())
-                        })
-                        .collect::<Vec<_>>();
-
-                    Some(format!(
-                        "{}\n{}",
-                        album_dir.to_str().unwrap(),
-                        copied_files.join("\n"),
-                    ))
-                }
-            } else {
-                None
-            }
-        })
+        .filter_map(|path| copy_album_dir_contents(&target_dir, path))
         .collect::<Vec<_>>();
 
     println!("==================================================");
@@ -128,4 +129,7 @@ fn main() {
     } else {
         println!("All up-to-date");
     }
+    let end = SystemTime::now();
+
+    println!("Took {}ms", end.duration_since(start).unwrap().as_millis());
 }
